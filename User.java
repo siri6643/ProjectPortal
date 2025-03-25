3bac647c72c8361b7634c63e6de26c6e1346dd47
Siri Chandana <23wh1a6643@bvrithyderabad.edu.in>
2025-03-25 18:34:52 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,50 @@
+package com.model;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long userId;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+    
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+    
+}
